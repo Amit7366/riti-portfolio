04e62903795c38e11e980c26955c6361627c96a8
Amit Singh <omit9090@gmail.com>
2023-09-23 13:00:01 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+AOS.init();
+var swiper = new Swiper(".mySwiper", {
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+  });
